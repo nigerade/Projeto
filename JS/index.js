0ff25c2e2deed489ab270a496312393405ad4e49
Nicholas Gerade <nicholasgerade@gmail.com>
2023-10-23 10:48:00 -0300

    Changes

--- JS/index.js
+++ JS/index.js
@@ -3,5 +3,5 @@ var elementosDuvida = document.querySelectorAll('.duvida')
 elementosDuvida.forEach(function (duvida) {
     duvida.addEventListener('click', function () {
         duvida.classList.toggle('ativa')
-    })    
+    })
 })
