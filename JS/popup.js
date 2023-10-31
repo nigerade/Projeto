51d5adced11e0e2c228be58acb5e661d8a93586e
Nicholas Gerade <nicholasgerade@gmail.com>
2023-10-31 16:12:16 -0300

    Changes Thank You page

--- JS/popup.js
+++ JS/popup.js
@@ -1,22 +1,24 @@
-document.getElementById("open-popup-btn").addEventListener("click", function (event) {
-    event.preventDefault(); // Impede o envio do formulário padrão
+document.querySelector(".open-popup-btn").addEventListener("click", function (event) {
     var nome = document.getElementById("campo__nome").value;
     var email = document.getElementById("campo__email").value;
     var mensagem = document.getElementById("campo__msg").value;
     var popup = document.getElementsByClassName("popup")[0];
 
     if (nome && email && mensagem) {
-        document.getElementById("open-popup-btn").style.display = "none";
-        popup.querySelector(".icon").classList.remove("error"); // Remove o estilo de erro
-        popup.querySelector(".icon i.fa").classList.remove("fa-times"); // Remove o ícone de erro
-        popup.querySelector(".icon i.fa").classList.add("fa-check"); // Adiciona o ícone de check
+        // Todos os campos estão preenchidos, permita o envio do formulário
+        document.querySelector(".open-popup-btn").style.display = "none";
+        popup.querySelector(".icon").classList.remove("error");
+        popup.querySelector(".icon i.fa").classList.remove("fa-times");
+        popup.querySelector(".icon i.fa").classList.add("fa-check");
         popup.querySelector(".title").textContent = "Email enviado com Sucesso";
         popup.querySelector(".description").textContent = "Nossa equipe receberá o seu email com suas informações e questionamentos";
         popup.classList.add("active");
     } else {
-        popup.querySelector(".icon").classList.add("error"); // Adiciona o estilo de erro
-        popup.querySelector(".icon i.fa").classList.remove("fa-check"); // Remove o ícone de check
-        popup.querySelector(".icon i.fa").classList.add("fa-times"); // Adiciona o ícone de erro
+        // Campos estão vazios, evite o envio do formulário e mostre uma mensagem de erro
+        event.preventDefault();
+        popup.querySelector(".icon").classList.add("error");
+        popup.querySelector(".icon i.fa").classList.remove("fa-check");
+        popup.querySelector(".icon i.fa").classList.add("fa-times");
         popup.querySelector(".title").textContent = "Por favor, preencha todos os campos!";
         popup.querySelector(".description").textContent = "";
         popup.classList.add("active");
@@ -24,6 +26,6 @@ document.getElementById("open-popup-btn").addEventListener("click", function (ev
 });
 
 document.getElementById("dismiss-popup-btn").addEventListener("click", function () {
-    document.getElementById("open-popup-btn").style.display = "block";
-    document.getElementsByClassName("popup")[0].classList.remove("active");
+    document.querySelector(".open-popup-btn").style.display = "block";
+    document.querySelector(".popup").classList.remove("active");
 });
