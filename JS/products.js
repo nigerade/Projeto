7b1040ec814c0835a624f9ab5a01b7b53cafc6e0
Nicholas Gerade <nicholasgerade@gmail.com>
2023-11-07 19:59:45 -0300

    changes products page

--- JS/products.js
+++ JS/products.js
@@ -1,18 +1,8 @@
 var MainImg = document.getElementById('MainImg');
 var small_Img = document.getElementsByClassName('small-img');
 
-small_Img[0].onclick = function() {
-    MainImg.src = small_Img[0].scr;
-}
-
-small_Img[1].onclick = function() {
-    MainImg.src = small_Img[1].scr;
-}
-
-small_Img[2].onclick = function() {
-    MainImg.src = small_Img[2].scr;
-}
-
-small_Img[3].onclick = function() {
-    MainImg.src = small_Img[3].scr;
+for (var i = 0; i < small_Img.length; i++) {
+    small_Img[i].onclick = function() {
+        MainImg.src = this.src;
+    }
 }
